409fba5ceed66cdcf3261ac71d6cee12e45f4391
Sophia Shaikh <sophias930@gmail.com>
2019-04-19 22:14:00 -0400

    camera added
    now we can take picccctttuuurrreessss

--- app/src/main/java/com/example/baddiemanager/MainActivity.java
+++ app/src/main/java/com/example/baddiemanager/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.baddiemanager;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    Bitmap bitty = null;
+    byte[] byteArray = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         EditText passField = findViewById(R.id.pass);
         String password = passField.getText().toString();
 
+        photoPage(v);
+    }
+
+    public void photoPage(View v) {
         setContentView(R.layout.photo_capture);
     }
 
@@ -28,10 +43,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void camera() {
-
+    public void camera(View v) {
+        Intent x = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(x, 1);
     }
 
+    protected void onActivityResult(int rc, int resc, Intent data) {
+        ImageView iv = null;
+        Bitmap bm = (Bitmap) data.getExtras().get("data");
+        iv = ((ImageView) findViewById(R.id.theView));
+        iv.setBackgroundResource(0);
+        iv.setImageBitmap(bm);
+
+        bitty = ((BitmapDrawable) iv.getDrawable()).getBitmap();
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitty.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byteArray = stream.toByteArray();
+    }
 
 
 }
